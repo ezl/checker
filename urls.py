d785fd848d33cbc3ca43d7e5cd89800a1f7cb02b
ezl <ericzliu@gmail.com>
2011-01-13 09:53:13 -0600

    init

---
+++ urls.py
@@ -0,0 +1,21 @@
+from django.conf import settings
+from django.conf.urls.defaults import *
+
+# from django.contrib import admin
+# admin.autodiscover()
+
+
+urlpatterns = patterns('',
+    url(r'^$', 'main.views.homepage', name='homepage'),
+    url(r'^trade/$', 'main.views.trade', name='trade'),
+    url(r'^position/$', 'main.views.position', name='position'),
+)
+
+# dev niceties
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^site_media/(?P<path>.*)$', 'django.views.static.serve',
+         {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
+#        (r'^raw_template/(?P<template>.*)', 'django.views.generic.simple.direct_to_template'),
+#        (r'^messages_test/$', 'main.views.messages_test'),
+    )
