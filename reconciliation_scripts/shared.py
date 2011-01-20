e59685962f4ec48438dcc15bcb51f53964915dc0
ezl <ericzliu@gmail.com>
2011-01-20 17:52:36 -0600

    start building common infrastructure

---
+++ reconciliation_scripts/shared.py
@@ -0,0 +1,19 @@
+
+class Trade(object):
+    """A generic trade class.
+
+       Defines the common attributes that all trade objects must have for
+       comparison across different clearing firms and trading software"""
+
+    def __init__(self, contract_name, contract_type, quantity, price)
+        self.contract_name = contract_name
+        self.contract_type = contract_type
+        self.quantity = quantity
+        self.price = price
+        # executing broker, conterparty, house, house, exchange, electronic, time, spread, blah blah
+
+class Position(object):
+    def __init__(self, contract_name, contract_type, quantity)
+        self.contract_name = contract_name
+        self.contract_type = contract_type
+        self.quantity = quantity
