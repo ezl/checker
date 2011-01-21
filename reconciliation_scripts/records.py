29dca7f61fc6eabaf57aa6823da9cdd040380329
ezl <ericzliu@gmail.com>
2011-01-21 14:46:00 -0600

    Don't require contract_type to init a Trade object
    contract type determination can happen internally in the class
    during the __init__()

--- reconciliation_scripts/records.py
+++ reconciliation_scripts/records.py
@@ -5,12 +5,11 @@ class Trade(object):
        Defines the common attributes that all trade objects must have for
        comparison across different clearing firms and trading software"""
 
-    def __init__(self, contract_name, contract_type, quantity, price)
+    def __init__(self, contract_name, quantity, price)
         self.contract_name = contract_name
-        self.contract_type = contract_type
         self.quantity = quantity
         self.price = price
-        # executing broker, conterparty, house, house, exchange, electronic, time, spread, blah blah
+        # contract_type, executing broker, conterparty, house, house, exchange, electronic, time, spread, blah blah
 
 class Position(object):
     def __init__(self, contract_name, contract_type, quantity)
