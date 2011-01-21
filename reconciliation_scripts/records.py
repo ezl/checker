da523395fb5237ccf680278b79e941e2048aca7e
ezl <ericzliu@gmail.com>
2011-01-21 14:47:55 -0600

    don't require contract_type for Position object

--- reconciliation_scripts/records.py
+++ reconciliation_scripts/records.py
@@ -5,14 +5,17 @@ class Trade(object):
        Defines the common attributes that all trade objects must have for
        comparison across different clearing firms and trading software"""
 
-    def __init__(self, contract_name, quantity, price)
+    def __init__(self, contract_name, quantity, price, **kwargs)
         self.contract_name = contract_name
         self.quantity = quantity
         self.price = price
         # contract_type, executing broker, conterparty, house, house, exchange, electronic, time, spread, blah blah
 
 class Position(object):
-    def __init__(self, contract_name, contract_type, quantity)
+    """A generic position class.
+
+       Defines common attributes of a position object"""
+
+    def __init__(self, contract_name, quantity, **kwargs)
         self.contract_name = contract_name
-        self.contract_type = contract_type
         self.quantity = quantity
